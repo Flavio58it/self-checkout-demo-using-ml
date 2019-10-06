c29973fe2f4dab7b1b60f8821e990a8b6098f731
Avinashkr1612 <avinashkr1612@gmail.com>
2019-10-07 00:01:10 +0530

    Some Changes

--- js/script.js
+++ js/script.js
@@ -11,3 +11,9 @@
 //     function videoError(e){
 
 //     }
+
+document.getElementById('capture').addEventListener('click',function(){
+    context = canvas.getContext('2d')
+    context.drawImage(video,0,0,400,300);
+
+});
