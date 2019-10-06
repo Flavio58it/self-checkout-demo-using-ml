d908af14bc2d26471fd4ca35cf05f79b5a968561
Kumar Anurag <kmranrg@gmail.com>
2019-10-06 23:32:52 +0530

    added invoice

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-// var video = document.querySelector("#videoElement");
-// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+	// var video = document.querySelector("#videoElement");
+	// navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
 
-// if (navigator.getUserMedia) {
-//     navigator.getUserMedia({ video: true },handleVideo,videoError);
-//     }
-    
-//     function handleVideo(stream){
-//         video.srcObject = stream;
-//     }
-//     function videoError(e){
+	// if (navigator.getUserMedia) {
+	//     navigator.getUserMedia({ video: true },handleVideo,videoError);
+	//     }
+	    
+	//     function handleVideo(stream){
+	//         video.srcObject = stream;
+	//     }
+	//     function videoError(e){
 
-//     }
+	//     }
